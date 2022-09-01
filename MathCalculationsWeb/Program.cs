b054f1dcb8ed1689341fbe95efd61e367d2756e6
Dan Vigliotti <dan.vigliotti@webvig.com>
2022-09-01 09:45:47 -0400

    Initial Commit

---
+++ MathCalculationsWeb/Program.cs
@@ -0,0 +1,32 @@
+﻿using MathFunctions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/add", () =>
+{
+    // Declare Functions class which is available in MathFunctions.Dll
+    Functions mathFunctions = new Functions();
+
+    // Access Add method from the class library
+    var add = mathFunctions.Add(20, 10);
+
+    return add;
+});
+
+app.Run();
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
