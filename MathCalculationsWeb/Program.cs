e80517148fdcaa906ea5f6539bd38199b578c5f3
Dan Vigliotti <dan.vigliotti@webvig.com>
2022-09-01 09:54:17 -0400

    Simple change

--- MathCalculationsWeb/Program.cs
+++ MathCalculationsWeb/Program.cs
@@ -8,11 +8,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/add", () =>
 {
     // Declare Functions class which is available in MathFunctions.Dll
@@ -25,8 +20,3 @@ app.MapGet("/add", () =>
 });
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
